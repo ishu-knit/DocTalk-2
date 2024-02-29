7c89b81190712daca675d188281441db41766248
ishu-knit <123448281+ishu-knit@users.noreply.github.com>
2024-02-29 19:18:32 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+# (successfully run + giving ans to text only not other info)
+
+from langchain_community.vectorstores import FAISS    
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.llms import Ollama
+
+
+ollama_embeddings = OllamaEmbeddings(model="zephyr",base_url = 'http://127.0.0.1:11434')
+model = Ollama(model="zephyr",base_url = 'http://127.0.0.1:11434')
+
+
+# file path
+doc_reader = PyPDFLoader("prompt.pdf")
+
+docs = doc_reader.load_and_split()
+
+# create model
+from langchain_community.llms import Ollama
+model = Ollama(model="zephyr",base_url = 'http://127.0.0.1:11434')
+# make vectorstore
+vectorstore = FAISS.from_documents(docs,ollama_embeddings)
+print("vectore stored")
+# retreival qa
+from langchain.chains import RetrievalQA
+chain = RetrievalQA.from_chain_type(model,retriever=vectorstore.as_retriever())
+
+query = input("type your query:- ")
+while query:
+# query and its answer
+    ans =chain.invoke({"query":query})
+    print("ans:-->", ans)
+    query = input("type your query to continue / press enter to end conversation :- ")
